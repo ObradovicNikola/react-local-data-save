7a4b3f19bdcbc86c20d4abe112bf26dd79bbee11
ObradovicNikola <obradovicnikola009@gmail.com>
2019-09-23 14:41:57 +0200

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-local-data-save
-using localStorage and sessionStorage in react to save data localy / per session
---
+++ precache-manifest.7e90738e07df25e558d14a864d94b9eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d040ede74c554d1082f0efd7e50d33a6",
+    "url": "/react-local-data-save/index.html"
+  },
+  {
+    "revision": "53c170b4670aca4ddf76",
+    "url": "/react-local-data-save/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "b43b521f3093473e999d",
+    "url": "/react-local-data-save/static/js/2.b7f8c1f8.chunk.js"
+  },
+  {
+    "revision": "53c170b4670aca4ddf76",
+    "url": "/react-local-data-save/static/js/main.ca12e373.chunk.js"
+  },
+  {
+    "revision": "0ecb5c949cc0e0b003a7",
+    "url": "/react-local-data-save/static/js/runtime-main.b6ebaf25.js"
+  }
+]);
